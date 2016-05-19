870bcd784eecfd0198a88c9e6616ee574535fba3
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-05-19 14:48:57 +0200

    Show output

--- qgis-desktop-docker/qgis_testrunner.sh
+++ qgis-desktop-docker/qgis_testrunner.sh
@@ -5,11 +5,11 @@
 
 TEST_NAME=$1
 
-pushd .
 cd /tests_directory
-unbuffer qgis --optionspath /qgishome --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null | grep FAILED
+OUTPUT=`unbuffer qgis --optionspath /qgishome --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null`
+echo $OUTPUT | grep -q FAILED
 RETURN_CODE=$?
-popd
+echo "$OUTPUT"
 if [ $RETURN_CODE != 0 ];
     then exit 0;
 fi
