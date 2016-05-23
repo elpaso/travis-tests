4b8a99a67c2230a58e88e51127781951627c9a91
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-05-23 16:57:50 +0200

    Added message on empty output

--- qgis-desktop-docker/qgis_testrunner.sh
+++ qgis-desktop-docker/qgis_testrunner.sh
@@ -6,10 +6,16 @@
 TEST_NAME=$1
 
 cd /tests_directory
+echo "Running test $1 ..."
 OUTPUT=`unbuffer qgis --optionspath /qgishome --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null`
 echo $OUTPUT | grep -q FAILED
+if [ -z "$OUTPUT" ]; then
+    echo "ERROR: no output from the test runner!"
+    exit 1
+fi
 RETURN_CODE=$?
 echo "$OUTPUT"
+echo "Finished running test $1 ..."
 if [ $RETURN_CODE != 0 ];
     then exit 0;
 fi
