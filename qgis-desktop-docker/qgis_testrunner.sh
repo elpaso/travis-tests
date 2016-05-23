4ed69eff9076a8f13e3bf676950d911b0fb2d9b6
Alessandro Pasotti <elpaso@itopen.it>
2016-05-23 13:56:07 +0200

    more output

--- qgis-desktop-docker/qgis_testrunner.sh
+++ qgis-desktop-docker/qgis_testrunner.sh
@@ -4,12 +4,14 @@
 #set -x
 
 TEST_NAME=$1
+echo "Running test $1"
 
 cd /tests_directory
 OUTPUT=`unbuffer qgis --optionspath /qgishome --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null`
 echo $OUTPUT | grep -q FAILED
 RETURN_CODE=$?
 echo "$OUTPUT"
+echo "End running test $1"
 if [ $RETURN_CODE != 0 ];
     then exit 0;
 fi
